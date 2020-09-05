6e756dc7a3268422dbebf7fd46324e891b5de337
Lakshya1457 <62178419+Lakshya1457@users.noreply.github.com>
2020-09-05 13:25:22 +0530

    Add files via upload

---
+++ AngryBirdsStage8-master/sketch.js
@@ -0,0 +1,115 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    
+    pig1 = new Pig(810, 350);
+    hippo1 = new hippo(770,350);
+    hippo2 = new hippo(700,350);
+    hippo3 = new hippo(900,350);
+    
+
+   
+   
+    pig3 = new Pig(810, 220);
+
+    
+
+  
+    
+
+    bird = new Bird(200,50);
+
+    
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    
+    ground.display();
+    pig1.display();
+    pig1.score();
+   
+
+  
+    pig3.display();
+    pig3.score();
+   hippo1.display();
+   hippo2.display();
+   hippo3.display();
+   
+    
+
+    bird.display();
+    platform.display();
+   
+      
+}
+
+function mouseDragged(){
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+     slingshot.attach(bird.body);
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
